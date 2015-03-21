663367a7fb6f169079bb9d0ead6fec6f9aded769
Mithrandir0x <oriol.lopez.sanchez@gmail.com>
2015-03-21 13:06:01 +0100

    Fixed example kernels

---
+++ OpenCL/kernel/matmul_singlewg_kernel.cl
@@ -0,0 +1,31 @@
+/**
+ * Matrix multiplication with an NDRange with one Work Item.
+ * Both A, B and C must have MS width and height.
+ * 
+ * @param *A Pointer to matrix A
+ * @param *B Pointer to matrix B
+ * @param *C Pointer to matrix C where the result will be stored
+ * @param MS Matrix size
+ */
+
+float doAPoint(int x, int y, __global float* A, __global float *B, const int sizeAX, const int sizeBX) {
+    float result = 0;
+    int posA, posB;
+
+    for ( int step = 0 ; step < sizeAX ; step++ ) {
+        posA = ( y * sizeAX ) + step;
+        posB = ( step * sizeBX ) + x;
+
+        result = result + A[posA] * B[posB];
+    }
+    
+    return result;
+}
+
+__kernel void MatMulKernel(__global float *A, __global float *B, __global float *C, int MS ) {
+    for ( int y = 0 ; y < MS ; y++ ) {
+        for ( int x = 0 ; x < MS ; x++ ) {
+            C[( y * MS ) + x] = doAPoint(x, y, A, B, MS, MS);
+        }
+    }
+}
--- OpenCL/main.c
+++ OpenCL/main.c
@@ -16,7 +16,7 @@ NDRANGE    = compute device?
 #define BLOCK_SIZE_H 1
 #define BLOCK_SIZE_V 1
 #define ALPHA 0.0001
-#define MATRIX_SIZE 16
+#define MATRIX_SIZE 4
 
 /* STRUCT HELPERS */
 typedef struct {
@@ -38,6 +38,22 @@ void getSoftware(sclHard hardware, SimpleKernelDefinition* def) {
 		hardware);
 }
 
+void printHexChar(char* s, int l) {
+	for ( int i = 0 ; i < l ; i++ ) {
+		printf(" %c (0x%02x)", s[i], s[i]);
+	}
+	printf(" ");
+}
+
+void printMatrix(float* M, int matrixSize) {
+	for ( int y = 0 ; y < matrixSize ; y++ ) {
+		for ( int x = 0 ; x < matrixSize ; x++ ) {
+			printf("%f\t", M[y * matrixSize + x]);
+		}
+		printf("\n");
+	}
+}
+
 /* Matrix multiplication - Host code */
 /* Matrix dimensions are assumed to be multiples of BLOCK_SIZE */
 float doAPoint(int x, int y, float* A, float *B, const int sizeAX, const int sizeBX) {
@@ -57,34 +73,39 @@ float doAPoint(int x, int y, float* A, float *B, const int sizeAX, const int siz
 }
 
 void calculateTestMatrix(float* A, float* B, float* Ctest, int matrixSize) {
-	printf("\nCalculant resultats a CPU\n");
+	// printf("\nCalculant resultats a CPU\n");
 	for (int y = 0; y < matrixSize; y++) {
 		for (int x = 0; x < matrixSize; x++) {
 			Ctest[(y*matrixSize) + x] = doAPoint(x, y, A, B, matrixSize, matrixSize);
-			printf("%f\t", Ctest[(y*MATRIX_SIZE) + x]);
+			// printf("%f\t", Ctest[(y*MATRIX_SIZE) + x]);
 		}
-		printf("\n");
+		// printf("\n");
 	}
 }
 
-void checkMatrixMultiplication(float* C, float* Ctest, int elements, float alpha) {
+int checkMatrixMultiplication(float* C, float* Ctest, int elements, float alpha) {
 	float diff = 0.0;
 	int count = 0;
+	int result = 0;
 	for ( int i = 0 ; i < elements ; i++ ) {
 		diff = C[i] - Ctest[i];
 
 		if ( diff > alpha ) {
 			count++;
 			printf("\nError a la posicio %d de C. Valor de C = %f. Valor de Ctest= %f.", i, C[i], Ctest[i]);
+			result = 1;
 		}
 	}
+
+	return result;
 }
 
-void MatrixMultiplication(sclHard hardware, char* module, char* kernel) {
+void MatrixMultiplicationSingleWorkItem(sclHard hardware, char* module, char* kernel) {
 	SimpleKernelDefinition d;
 
-	const int matrixSize = MATRIX_SIZE;
+	const int matrixSize = 4;
 	const int localSize = 1;
+	int result;
 
 	/* Host data */
 	float *A = NULL;     // Input array
@@ -104,37 +125,73 @@ void MatrixMultiplication(sclHard hardware, char* module, char* kernel) {
 	size_t datasize = sizeof(float) * elements;
 
 	/* Allocate space for input/output data */
-	A = (float*)malloc(datasize);
-	B = (float*)malloc(datasize);
-	C = (float*)malloc(datasize);
-	Ctest = (float*)malloc(datasize);
+	A = (float*) malloc(datasize);
+	B = (float*) malloc(datasize);
+	C = (float*) malloc(datasize);
+	Ctest = (float*) malloc(datasize);
 
 	/* Initialize the input data */
 	for ( int i = 0 ; i < elements ; i++ ) {
 		A[i] = i * 0.00001;
 		B[i] = 0.001;
 	}
+  	
+  	printf("A:\n");
+	printMatrix(A, matrixSize);
+  	printf("\n");
+  	
+  	printf("B:\n");
+	printMatrix(B, matrixSize);
+	printf("\n");
+
+	/* Initialize test matrix */
+	calculateTestMatrix(A, B, Ctest, matrixSize);
+  	
+  	printf("Ctest:\n");
+	printMatrix(Ctest, matrixSize);
+	printf("\n");
 
 	// Define an NDRange size of 16x16 Work Items
-	d.globalSize[0] = matrixSize;
-	d.globalSize[1] = matrixSize;
+	d.globalSize[0] = 1;
+	d.globalSize[1] = 1;
 
 	// Define the Work Group size that will contain the Work Items
-	d.localSize[0] = localSize;
-	d.localSize[1] = localSize;
+	d.localSize[0] = 1;
+	d.localSize[1] = 1;
 
 	/* Compile the kernel to the target device */
+	printf("Loading kernel\n");
 	getSoftware(hardware, &d);
 
 	/* Execute the compiled kernel */
+	printf("Executing kernel\n");
+	sclManageArgsLaunchKernel(
+		d.hardware, d.software,
+		d.globalSize, d.localSize,
+		"%r %r %w %a",
+		datasize, A,
+		datasize, B,
+		datasize, C,
+		sizeof(int), &matrixSize);
+	printf("\n");
 
+  	printf("C:\n");
+	printMatrix(C, matrixSize);
+	printf("\n");
 
 	/* Verify matrix multiplication */
-	checkMatrixMultiplication(C, Ctest, MATRIX_SIZE, ALPHA);
+	printf("Verifying matrix multiplication\n");
+	result = checkMatrixMultiplication(C, Ctest, MATRIX_SIZE, ALPHA);
+
+	if ( result ) {
+		printf("\n");
+	} else {
+		printf("All good\n");
+	}
 }
 
 /* The SimpleCL Hello World example, though utterly simplified (or vilified) */
-void ReadWrite(sclHard hardware) {
+void ReadWrite(sclHard hardware, char* module, char* kernel) {
 	SimpleKernelDefinition d;
 
 	/* Host data */
@@ -142,11 +199,11 @@ void ReadWrite(sclHard hardware) {
 	char* out;
 	int variableLength;
 
-	d.modulePath = "./../OpenCL/read_write_kernel.cl";
-	d.kernelName = "ReadAndWriteKernel";
+	d.modulePath = module;
+	d.kernelName = kernel;
 
 	variableLength = strlen(in);
-	out = malloc(sizeof(char) * variableLength);
+	out = (char *) malloc(sizeof(char) * variableLength);
 
 	d.globalSize[0] = variableLength;
 	d.globalSize[1] = 1;
@@ -157,6 +214,7 @@ void ReadWrite(sclHard hardware) {
 	printf("Loading kernel\n");
 	getSoftware(hardware, &d);
 
+	printf("Executing kernel\n");
 	sclManageArgsLaunchKernel(
 		d.hardware, d.software,
 		d.globalSize, d.localSize,
@@ -164,26 +222,34 @@ void ReadWrite(sclHard hardware) {
 		variableLength, in,
 		variableLength, out);
 
-	puts(out);
+	//printf("[%s]\n", out);
+	printf("Copied string: ");
+	printf("[");
+	printHexChar(out, variableLength);
+	printf("]\n");
 	
 	free(out);
 }
 
 int main() {
-  /* This code executes on the OpenCL host */
-  int found;
+	/* This code executes on the OpenCL host */
+	int found;
 
-  /* SimpleOpenCL types declaration */
-  sclHard* hardware;
+	/* SimpleOpenCL types declaration */
+	sclHard* hardware;
+	sclHard selected;
 
-  /* Initialize kernel */
-  //hardware = sclGetGPUHardware( 0, &found );
-  hardware = sclGetAllHardware(&found);
+	/* Initialize kernel */
+	//hardware = sclGetGPUHardware( 0, &found );
+	hardware = sclGetAllHardware(&found);
+	selected = hardware[1];
+	printf("\n\n");
 
-  ReadWrite(hardware[0]);
-  
-  printf("\n");
-  
-  return 0;
+	// ReadWrite(selected, "./kernel/read_write_kernel.cl", "ReadAndWriteKernel");
+	MatrixMultiplicationSingleWorkItem(selected, "./kernel/matmul_singlewg_kernel.cl", "MatMulKernel");
+
+	printf("\n");
+
+	return 0;
 }
 
